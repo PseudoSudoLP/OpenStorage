3977aab3f5361713f36c45457579de746568f0ea
PseudoSudoLP <pseudo.sudo.lp@gmail.com>
2015-03-09 23:11:05 -0400

    Add basic mod class structure.

--- src/main/java/com/pseudo_sudo/openstorage/OpenStorage.java
+++ src/main/java/com/pseudo_sudo/openstorage/OpenStorage.java
@@ -1,5 +1,10 @@
 package com.pseudo_sudo.openstorage;
 
-public class OpenStorage {
+import cpw.mods.fml.common.Mod;
 
+@Mod(modid=OpenStorage.ID, name=OpenStorage.NAME, version=OpenStorage.VERSION)
+public class OpenStorage {
+    public static final String ID = "openstorage";
+    public static final String NAME = "OpenStorage";
+    public static final String VERSION = "1.7.10-0.1";
 }
