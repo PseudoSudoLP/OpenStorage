3c9785a80e438a00f5f0f8253ada583e9f3fbe31
PseudoSudoLP <pseudo.sudo.lp@gmail.com>
2015-03-09 23:17:03 -0400

    Create ConfigurationController on preInit.

--- src/main/java/com/pseudo_sudo/openstorage/OpenStorage.java
+++ src/main/java/com/pseudo_sudo/openstorage/OpenStorage.java
@@ -1,10 +1,19 @@
 package com.pseudo_sudo.openstorage;
 
+import com.pseudo_sudo.openstorage.configuration.ConfigurationController;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid=OpenStorage.ID, name=OpenStorage.NAME, version=OpenStorage.VERSION)
 public class OpenStorage {
     public static final String ID = "openstorage";
     public static final String NAME = "OpenStorage";
     public static final String VERSION = "1.7.10-0.1";
+
+    private ConfigurationController configurationController;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        this.configurationController = new ConfigurationController(event.getSuggestedConfigurationFile());
+    }
 }
