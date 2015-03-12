fc98b4a0fcade75ab21d5b4a4a0609d33dd87ec7
PseudoSudoLP <pseudo.sudo.lp@gmail.com>
2015-03-12 19:33:38 -0400

    Change mod ID.

--- src/main/java/com/pseudo_sudo/openstorage/OpenStorage.java
+++ src/main/java/com/pseudo_sudo/openstorage/OpenStorage.java
@@ -6,7 +6,8 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid=OpenStorage.ID, name=OpenStorage.NAME, version=OpenStorage.VERSION)
 public class OpenStorage {
-    public static final String ID = "openstorage";
+    public static final String ID = "OpenStorage";
+    public static final String ID_LOWER = ID.toLowerCase();
     public static final String NAME = "OpenStorage";
     public static final String VERSION = "1.7.10-0.1";
 
