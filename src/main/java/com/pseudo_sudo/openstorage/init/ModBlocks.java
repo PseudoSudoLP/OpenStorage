ed4cbad27045cdc9bfef5847a513e19be0b0f18f
PseudoSudoLP <pseudo.sudo.lp@gmail.com>
2015-03-13 09:20:06 -0400

    Add init package with class to init blocks.

---
+++ src/main/java/com/pseudo_sudo/openstorage/init/ModBlocks.java
@@ -0,0 +1,14 @@
+package com.pseudo_sudo.openstorage.init;
+
+import com.pseudo_sudo.openstorage.block.BlockNAS;
+import com.pseudo_sudo.openstorage.util.Name;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+
+public class ModBlocks {
+    private static final Block blockNAS = new BlockNAS();
+
+    public static void init() {
+        GameRegistry.registerBlock(blockNAS, Name.Blocks.NAS.getQualifiedBlockName());
+    }
+}
