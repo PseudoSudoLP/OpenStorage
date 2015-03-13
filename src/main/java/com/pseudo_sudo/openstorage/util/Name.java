14156f78e4abe098eab54d61903451ce63ebd36a
PseudoSudoLP <pseudo.sudo.lp@gmail.com>
2015-03-13 09:16:48 -0400

    Add uitl package with Name class.

---
+++ src/main/java/com/pseudo_sudo/openstorage/util/Name.java
@@ -0,0 +1,19 @@
+package com.pseudo_sudo.openstorage.util;
+
+import com.pseudo_sudo.openstorage.OpenStorage;
+
+public class Name {
+    public static enum Blocks {
+        NAS("nas");
+
+        private String blockName;
+
+        Blocks(String blockName) {
+            this.blockName = blockName;
+        }
+
+        public String getQualifiedBlockName() {
+            return OpenStorage.ID_LOWER + ':' + this.blockName;
+        }
+    }
+}
