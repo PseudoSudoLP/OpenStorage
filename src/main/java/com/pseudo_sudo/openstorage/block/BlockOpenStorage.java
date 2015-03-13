f926a50d6425863eece6235963dad66072300be1
PseudoSudoLP <pseudo.sudo.lp@gmail.com>
2015-03-12 20:52:08 -0400

    Add base block.

---
+++ src/main/java/com/pseudo_sudo/openstorage/block/BlockOpenStorage.java
@@ -0,0 +1,10 @@
+package com.pseudo_sudo.openstorage.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockOpenStorage extends Block {
+    public BlockOpenStorage() {
+        super(Material.rock);
+    }
+}
